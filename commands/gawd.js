45405486dc3b12824985f3864a17eabcc0d0476c
Will Paceley <pace0033@algonquinlive.com>
2022-03-06 15:04:13 -0500

    set up initial fetch call to Gawds API

--- commands/gawd.js
+++ commands/gawd.js
@@ -1,15 +1,35 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gawd')
 		.setDescription('Displays a Gawd by ID')
+		// An option is an input from the user, e.g. to get the ID
 		.addIntegerOption(option =>
 			option.setName('id')
 				.setDescription('The ID for the Gawd to display')
 				.setRequired(true)),
+	// The function that will be executed on calling the command
 	async execute(interaction) {
 		const id = interaction.options.getInteger('id');
-		await interaction.reply(`Displaying Gawd #${id}`);
+		// Immediately deferReply() so interaction token doesn't expire during fetch
+		await interaction.deferReply();
+
+		try {
+			const { name } = await fetch(`https://www.gawds.xyz/api/gawds/${id}`)
+				.then(response => {
+					// Check if the API returns a Gawd for the ID specified
+					if (!response.ok) {
+						throw new Error(`No Gawd found with ID ${id}`);
+					}
+					return response.json();
+				});
+			// console.log(name);
+			await interaction.editReply(`Displaying Gawd #${id} ${name}`);
+		}
+		catch (error) {
+			await interaction.editReply(error.message);
+		}
 	},
 };
